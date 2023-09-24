1b260dc8a0a2eaadae52b8389215eab1a0d4cd65
abcdegon <egon@gruenter.eu>
2023-09-24 08:19:47 +0200

    first test with curses

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hackmac
-Some tools to be used for different security purposes.
+
+I started *hackmac* as a weekend project. The idea behind it was: <br>
+1. to stay in touch which practical security
+2. try to learn a new programming language (Python)
+3. try to get experience running Docker containers
+
+Every single tool will be documented here.
+
+## Setup
+Create a virtual environment.
+
+´´´
+sh>python3 -m venv hackenv
+sh>pip install --upgrade pip
+´´´
+
--- hackmac.py
+++ hackmac.py
@@ -1,26 +1,39 @@
 #!/usr/bin/enc python
-import os
+import curses
+from curses import wrapper
 
-min = 0
-max = 0
-c = 1
+def main(stdscr):
+    min = 0
+    max = 0
+    c = 1
 
-while c > 0:
-    os.system("clear")
-    print("+------------------------------+")
-    print("|       Hackmac by Abcd        |")
-    print("+------------------------------+")
-    print("| Exit ................... [0] |")
-    print("+------------------------------+")
-    print()
-    print("Your choice: ", end='')
-    try:
-        c = int(input())
-    except:
-        c = 99
-        print("Please enter a valid integer [",min,";",max,"]")
-        print("Press enter to continue")
-        _ = input()
-    
 
-print("Thank you for using Hackmac. Good bye!")
+    h = 7
+    w = 32
+    y = int( (curses.LINES-11)/2 )
+    x = int( (curses.COLS-32)/2 )
+    stdscr.clear()
+    win = curses.newwin(h,w,y,x)
+
+    while c > max:
+        win.clear()
+        win.border()
+        win.addstr(1,8,"Abcds Hackmac")
+        win.hline(2,1,0,30)
+        win.addstr(3,5,"Exit ............ [0]")
+        win.hline(4,1,0,30)
+        win.addstr(5,5,"Your choice [{},{}]".format(min,max))
+        z=win.getstr(5,23)
+        win.refresh()
+        try:
+            c=int(z)
+        except:
+            win.clear()
+            win.addstr(1,1,"Please enter a valid integer.")
+            win.getch()
+            win.refresh()
+
+
+if __name__ == "__main__" :
+    wrapper(main)
+
